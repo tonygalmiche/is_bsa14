700d108ca1ca16db8ca2b8295fd8f4295ca882eb
Tony Galmiche <tony.galmiche@infosaone.com>
2022-03-03 13:16:01 +0100

    modif

--- models/ir_mail_server.py
+++ models/ir_mail_server.py
@@ -4,9 +4,11 @@ from odoo import api, fields, models
 class IrMailServer(models.Model):
     _inherit = "ir.mail_server"
 
+    # Permet de mettre l'emeteur du mail en copie
     @api.model
     def send_email(self, message, mail_server_id=None, smtp_server=None, smtp_port=None,
-                   smtp_user=None, smtp_password=None, smtp_encryption=None, smtp_debug=False):
+                   smtp_user=None, smtp_password=None, smtp_encryption=None, smtp_debug=False,
+                   smtp_session=None):
         cc=''
         if message['From']:
             cc=message['From']

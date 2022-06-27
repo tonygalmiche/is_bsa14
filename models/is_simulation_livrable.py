033f1532ff1959b70c1ad160a26dcb5d0b81307b
Tony Galmiche <tony.galmiche@infosaone.com>
2022-06-27 11:02:16 +0200

    modifs

--- models/is_simulation_livrable.py
+++ models/is_simulation_livrable.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from itertools import product
 from odoo import models,fields,api
 import datetime
 from odoo.exceptions import Warning
@@ -17,14 +18,6 @@ class is_simulation_livrable(models.Model):
     demande_ids   = fields.One2many('is.simulation.livrable.ligne', 'simulation_id', 'Lignes', copy=True, domain=[('type_ligne', '=', 'demande')])
 
 
-        # 'move_lines': fields.one2many('stock.move', 'raw_material_production_id', 'Products to Consume',
-        #     domain=[('state', 'not in', ('done', 'cancel'))], readonly=True, states={'draft': [('readonly', False)]}),
-        # 'move_lines2': fields.one2many('stock.move', 'raw_material_production_id', 'Consumed Products',
-        #     domain=[('state', 'in', ('done', 'cancel'))], readonly=True),
-
-
-
-
     @api.model
     def create(self, vals):
         vals['name'] = self.env['ir.sequence'].next_by_code('is.simulation.livrable')
@@ -32,15 +25,26 @@ class is_simulation_livrable(models.Model):
         return res
 
 
-    def bom(self, demande, niv, product, qt):
-        print(demande,niv,"-"*niv,qt,product,product.name)
+    def bom(self, demande, niv, product, qt, stocks={}):
+        if product not in stocks:
+            stocks[product]=product.qty_available
+        stock = stocks[product]
+        besoin = qt-stock
+        if besoin<0:
+            besoin=0
+        reste = stocks[product] - qt
+        if reste<0:
+            reste=0
+        stocks[product]=reste
         vals = {
             'simulation_id': self.id,
             'demande_id'   : demande.id,
             'niv'          : niv,
             'niv_txt'      : "-"*niv,
             'product_id'   : product.id,
-            'qt'           : qt,
+            'demande'      : qt,
+            'stock'        : stock,
+            'besoin'       : besoin,
             'type_ligne'   : 'besoin_detaille',
         }
         res = self.env['is.simulation.livrable.ligne'].create(vals)
@@ -51,32 +55,47 @@ class is_simulation_livrable(models.Model):
         for bom in boms:
             niv+=1
             for line in bom.bom_line_ids:
-                self.bom(demande,niv,line.product_id,qt*line.product_qty)
+                stocks = self.bom(demande,niv,line.product_id,besoin*line.product_qty, stocks)
+        return stocks
 
 
     # TODO : 
     # - Tenir compte du stock ou du stock prévu (en multi-nivaux et ne compter le stock qu'un fois...)
-    # - Eclater la nomenlcatue sulment si c'est un produit à fabricque
-    # - Regrouper les besoins => Ajouter le stock sur chaque article et recalculaer en négatif les besoins sur les articles à fabriquer 
-    # - Indiquer les alertes demandées dans le CDC
-    # - Il faut peur-être faire un regroupement des besoins pour cahque niveau de nomenlcatue
-    # -- Regrouper les besoins du premeure niveau  => Tenir compte du stock
-    # -- Regrouper les besoins du niveau suivant tant qu'il y a des nomenlcatues à traiter
-    # -- Il ne faut donc pas faire du récurif mais un traitement niveau par niveaux x fois (10 niveux maxi)
 
 
     def calcul_besoins_action(self):
         for obj in self:
-            print(obj)
             filtre=[
                 ('simulation_id', '=' , obj.id),
                 ('type_ligne'   , '!=' , 'demande')
             ]
             self.env['is.simulation.livrable.ligne'].search(filtre).unlink()
-
             niv=1
             for demande in obj.demande_ids:
-                self.bom(demande,niv,demande.product_id,demande.qt)
+                self.bom(demande,niv,demande.product_id,demande.demande)
+
+            #** regroupement des besoins **************************************
+            filtre=[
+                ("simulation_id", "=", obj.id),
+                ("type_ligne"   , "=", "besoin_detaille"),
+            ]
+            lines = self.env['is.simulation.livrable.ligne'].search(filtre)
+            products={}
+            for line in lines:
+                if line.product_id not in products:
+                    products[line.product_id]=0
+
+                products[line.product_id]+=line.besoin
+            for product in products:
+                besoin = products[product]
+                if besoin>0:
+                    vals = {
+                        'simulation_id': self.id,
+                        'product_id'   : product.id,
+                        'besoin'       : besoin,
+                        'type_ligne'   : 'besoin_regroupe',
+                    }
+                    res = self.env['is.simulation.livrable.ligne'].create(vals)
 
 
 class is_simulation_livrable_ligne(models.Model):
@@ -90,7 +109,10 @@ class is_simulation_livrable_ligne(models.Model):
     niv           = fields.Char("Niv")
     niv_txt       = fields.Char("-")
     product_id    = fields.Many2one('product.product', 'Article', required=True)
-    qt            = fields.Float("Quantité")
+    simulation_livrable = fields.Boolean('Simulation livrable', related="product_id.is_simulation_livrable")
+    demande       = fields.Float("Demande", help="Demande initiale")
+    stock         = fields.Float("Stock"  , help="Stock restant")
+    besoin        = fields.Float("Besoin" , help="Demande - Stock" )
     type_ligne    = fields.Selection([
             ('demande'        , 'Demande'),
             ('besoin_detaille', 'Besoin détaillé'),

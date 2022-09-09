353fde5cf726771663617aa87caf007e0b19210b
Tony Galmiche <tony.galmiche@infosaone.com>
2022-09-09 13:07:57 +0200

    modifs

--- models/is_balance_agee.py
+++ models/is_balance_agee.py
@@ -5,50 +5,45 @@ from odoo.exceptions import Warning
 import unicodedata
 
 
-
 class is_balance_agee(models.Model):
     _name='is.balance.agee'
     _description='is.balance.agee'
     _order='date_creation desc'
     _rec_name = 'date_creation'
 
-    type_balance  = fields.Selection([('ventes', u'Ventes'),('achats', u'Achats')], "Type de balance", required=True)
-    avoir         = fields.Boolean("Inclure les avoirs")
-    date_creation = fields.Date("Date de création"         , required=True)
-    createur_id   = fields.Many2one('res.users', 'Créateur', required=True)
+    type_balance  = fields.Selection([('ventes', u'Ventes'),('achats', u'Achats')], "Type de balance", required=True, default="ventes")
+    avoir         = fields.Boolean("Inclure les avoirs"                   , default=True)
+    date_creation = fields.Date("Date de création"         , required=True, default=lambda *a: fields.Date.today())
+    createur_id   = fields.Many2one('res.users', 'Créateur', required=True, default=lambda self: self.env.user.id)
     ligne_ids     = fields.One2many('is.balance.agee.ligne', 'balance_id', u'Lignes')
 
-    _defaults = {
-        'type_balance' : 'ventes',
-        'avoir'        : True,
-        'date_creation': datetime.date.today(),
-        'createur_id'  : lambda obj, cr, uid, context: uid,
-    }
 
     def generer_lignes_action(self):
         for obj in self:
             res={}
-            now = datetime.datetime.now()
+            now = datetime.date.today()
             obj.ligne_ids.unlink()
             if obj.type_balance=='ventes':
-                type_facture=['out_invoice']
+                move_type=['out_invoice']
                 if obj.avoir:
-                    type_facture=['out_invoice', 'out_refund']
+                    move_type=['out_invoice', 'out_refund']
             else:
-                type_facture=['in_invoice']
+                move_type=['in_invoice']
                 if obj.avoir:
-                    type_facture=['in_invoice', 'in_refund']
+                    move_type=['in_invoice', 'in_refund']
             filter=[
-                ('state'       , 'in' , ['open']),
-                ('type'        , 'in' , type_facture)
+                ('state'       , 'in' , ['posted']),
+                ('move_type'   , 'in' , move_type)
             ]
-            invoices = self.env['account.invoice'].search(filter, order="partner_id,date_due,id")
-            for invoice in invoices:
-                date_due=datetime.datetime.strptime(invoice.date_due, '%Y-%m-%d')
-                delta  = (date_due - now).days + 1
-                residual   = invoice.residual
 
-                if invoice.type=="out_refund":
+
+            invoices = self.env['account.move'].search(filter, order="partner_id,invoice_date_due,id")
+            for invoice in invoices:
+                date_due = invoice.invoice_date_due
+                delta  = (date_due - now).days + 1
+                residual   = invoice.amount_residual
+
+                if invoice.move_type=="out_refund":
                     residual=-residual
 
 
@@ -70,11 +65,9 @@ class is_balance_agee(models.Model):
                 if delta<-120:
                     res[partner_id][6]+=residual
 
-                print(invoice.date_due,delta, invoice.partner_id.name,invoice.residual)
+                #print(invoice.invoice_date_due,delta, invoice.partner_id.name,invoice.amount_residual)
 
-            print(res)
             for line in res:
-                print(line)
                 vals={
                     'balance_id': obj.id,
                     'partner_id': line.id,

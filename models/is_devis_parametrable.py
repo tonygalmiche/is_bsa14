0479a5aefd56527edf8bed8955493ebadb55d03c
Tony Galmiche <tony.galmiche@infosaone.com>
2022-09-15 16:28:07 +0200

    modifs

--- models/is_devis_parametrable.py
+++ models/is_devis_parametrable.py
@@ -9,7 +9,7 @@ import re
 import openpyxl
 from glob import glob
 import logging
-from math import pi,tan,sqrt
+from math import pi,tan,sqrt, floor
 
 _logger = logging.getLogger(__name__)
 
@@ -968,8 +968,11 @@ class is_type_cuve_calcul(models.Model):
                 formule=formule.replace("ROUNDDOWN(","round(")
                 formule=formule.replace("ROUNDUP(","round(")
                 formule=formule.replace("ROUND(","round(")
-
                 formule=formule.replace("SQRT(","sqrt(")
+                formule=formule.replace("INT(","floor(")
+
+
+
 
 
                 # ** Traitement fonction IF ***********************************

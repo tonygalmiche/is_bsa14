71562f1612aeb3a35d6972aafbaa3c1d3a2885da
Tony Galmiche <tony.galmiche@infosaone.com>
2022-10-16 14:42:49 +0200

    modifs

--- models/is_sale_order_line.py
+++ models/is_sale_order_line.py
@@ -14,10 +14,13 @@ class is_sale_order_line(models.Model):
     partner_id             = fields.Many2one('res.partner', 'Client')
     is_categorie_client_id = fields.Many2one('is.categorie.client', string='Catégorie de client')
     product_id             = fields.Many2one('product.product', 'Article')
-    product_uom_qty        = fields.Float('Quantité'   , digits=(14,2))
-    price_unit             = fields.Float('Prix'       , digits=(14,2))
-    discount               = fields.Float('Remise (%)' , digits=(14,2))
-    montant_ht             = fields.Float('Montant HT' , digits=(14,2))
+    product_uom_qty        = fields.Float('Qt commandée', digits=(14,2))
+    qt_livree              = fields.Float('Qt livrée'   , digits=(14,2))
+    reste_a_livrer         = fields.Float('Reste à livrer'   , digits=(14,2))
+    price_unit             = fields.Float('Prix'        , digits=(14,2))
+    discount               = fields.Float('Remise (%)'  , digits=(14,2))
+    montant_ht             = fields.Float('Montant HT commandé'  , digits=(14,2))
+    montant_reste_a_livrer = fields.Float('Montant HT reste à livrer'  , digits=(14,2))
     is_date_prevue         = fields.Date("Date prévue")
     is_date_demandee       = fields.Date("Date demandée")
     state                  = fields.Char("Etat de la commande")
@@ -43,7 +46,10 @@ class is_sale_order_line(models.Model):
                     sol.product_uom_qty*sol.price_unit*(100-discount)/100 montant_ht,
                     sol.is_date_prevue,
                     sol.is_date_demandee,
-                    so.state
+                    so.state,
+                    COALESCE((select sum(sm.product_uom_qty) from stock_move sm where sm.sale_line_id=sol.id and sm.state='done'),0) qt_livree,
+                    (sol.product_uom_qty-COALESCE((select sum(sm.product_uom_qty) from stock_move sm where sm.sale_line_id=sol.id and sm.state='done'),0)) reste_a_livrer,
+                    (sol.product_uom_qty-COALESCE((select sum(sm.product_uom_qty) from stock_move sm where sm.sale_line_id=sol.id and sm.state='done'),0))*sol.price_unit*(100-discount)/100 montant_reste_a_livrer
                 from sale_order_line sol inner join sale_order so on sol.order_id=so.id
                                          inner join res_partner rp on so.partner_id=rp.id
                 where 
@@ -52,3 +58,4 @@ class is_sale_order_line(models.Model):
             );
         """)
 
+                    # 

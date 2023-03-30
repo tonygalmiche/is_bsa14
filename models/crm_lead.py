052fb2bda375586d760e144c80947f324215a11d
Tony Galmiche <tony.galmiche@infosaone.com>
2023-03-30 13:59:24 +0200

    modifs

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -31,6 +31,12 @@ class is_crm_lead_sortie(models.Model):
     commentaire    = fields.Char("Commentaire")
 
 
+class crm_stage(models.Model):
+    _inherit  = "crm.stage"
+
+    is_devis_envoye = fields.Boolean('Devis envoyé', default=False, help="Utilisé pour connaitre la durée de réponse aux demandes")
+
+
 class crm_lead(models.Model):
     _inherit  = "crm.lead"
 
@@ -38,6 +44,38 @@ class crm_lead(models.Model):
     def _compute_is_description_html(self):
         for obj in self:
             obj.is_description_html = (obj.is_description or '').replace('\n','<br />')
+
+    @api.depends('is_stage_ids')
+    def _compute_statistiques(self):
+        for obj in self:
+            is_date_premier_devis_envoye = False
+            is_duree_envoi_premier_devis = 0
+            is_date_dernier_traitement = False
+            is_duree_gagner = 0
+            is_duree_totale = 0
+            is_nombre_modifications = 0
+            date_start = False
+            for line in obj.is_stage_ids:
+                if not date_start:
+                    date_start = line.date_creation
+                if line.stage_id.is_devis_envoye and not is_date_premier_devis_envoye:
+                    is_date_premier_devis_envoye = line.date_creation
+                    is_duree_envoi_premier_devis = (line.date_creation.date() - date_start.date()).days
+                is_date_dernier_traitement = line.date_creation
+                if line.stage_id.is_won:
+                    is_duree_gagner = (line.date_creation.date() - date_start.date()).days
+                is_duree_totale = (line.date_creation.date() - date_start.date()).days
+                if line.stage_id.is_devis_envoye:
+                    is_nombre_modifications+=1
+            if is_nombre_modifications>0:
+                is_nombre_modifications-=1
+            obj.is_date_premier_devis_envoye = is_date_premier_devis_envoye
+            obj.is_duree_envoi_premier_devis = is_duree_envoi_premier_devis
+            obj.is_date_dernier_traitement = is_date_dernier_traitement
+            obj.is_duree_gagner = is_duree_gagner
+            obj.is_duree_totale = is_duree_totale
+            obj.is_nombre_modifications = is_nombre_modifications
+
 
     is_affaire_id          = fields.Many2one('is.devis.parametrable.affaire', 'Affaire')
     is_order_id            = fields.Many2one('sale.order', 'Commande client')
@@ -51,6 +89,14 @@ class crm_lead(models.Model):
     is_stage_ids  = fields.One2many('is.crm.lead.stage' , 'lead_id', "Etapes")
     is_entree_ids = fields.One2many('is.crm.lead.entree', 'lead_id', "Données d'entrée")
     is_sortie_ids = fields.One2many('is.crm.lead.sortie', 'lead_id', "Données de sortie")
+
+    is_date_premier_devis_envoye = fields.Date('Date premier devis envoyé'            , store=True, readonly=True, compute='_compute_statistiques')
+    is_duree_envoi_premier_devis = fields.Integer('Durée envoi premier devis (Jours)' , store=True, readonly=True, compute='_compute_statistiques')
+    is_date_dernier_traitement   = fields.Date('Date du dernier traitement'           , store=True, readonly=True, compute='_compute_statistiques')
+    is_duree_gagner              = fields.Integer('Durée pour gagner le devis (Jours)', store=True, readonly=True, compute='_compute_statistiques')
+    is_duree_totale              = fields.Integer('Durée totale de traitement (Jours)', store=True, readonly=True, compute='_compute_statistiques')
+    is_nombre_modifications      = fields.Integer('Nombre de modifications du devis'  , store=True, readonly=True, compute='_compute_statistiques', help="Nombre de fois ou le devis a été envoyé - 1")
+
 
     @api.model
     def create(self, vals):

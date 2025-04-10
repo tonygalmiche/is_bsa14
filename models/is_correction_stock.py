39afd09be3f10131f00fc2fbffb35640194443e8
Tony Galmiche <tony.galmiche@infosaone.com>
2025-04-10 13:20:47 +0200

    modifs

--- models/is_correction_stock.py
+++ models/is_correction_stock.py
@@ -2,6 +2,14 @@
 from odoo import models,fields,api
 
 
+class is_correction_stock_cause(models.Model):
+    _name='is.correction.stock.cause'
+    _description='Cause correction stock'
+    _order='name'
+
+    name = fields.Char("Cause", required=True)
+
+
 class is_correction_stock(models.Model):
     _name='is.correction.stock'
     _description='Correction stock'
@@ -9,15 +17,34 @@ class is_correction_stock(models.Model):
 
     name             = fields.Char("N°", readonly=True)
     product_id       = fields.Many2one('product.product', 'Article', required=True)
-    quantite         = fields.Integer("Quantité", required=True)
-    location_id      = fields.Many2one('stock.location', "Emplacement d'origine", required=True)
-    location_dest_id = fields.Many2one('stock.location', "Emplacement de destination", required=True)
+    quantite         = fields.Integer("Quantité à enlever", required=True)
+    location_id      = fields.Many2one('stock.location', "Emplacement d'origine"     , required=False, store=True, readonly=True, compute='_compute_location_id')
+    location_dest_id = fields.Many2one('stock.location', "Emplacement de destination", required=False, store=True, readonly=True, compute='_compute_location_id')
     move_id          = fields.Many2one('stock.move', 'Mouvement de stock', readonly=True, copy=False)
+    cause_id         = fields.Many2one('is.correction.stock.cause', 'Cause correction stock')
     commentaire      = fields.Char("Commentaire")
     state            = fields.Selection([
         ('creation', 'Création'),
         ('valide'  , 'Validé'),
     ], "État", required=True, copy=False, default='creation')
+
+
+    @api.depends('quantite')
+    def _compute_location_id(self):
+        for obj in self:
+            location_id = location_dest_id = False
+            locations=self.env['stock.location'].search([('usage' , '=' , 'internal')],limit=1)
+            for location in locations:
+                location_id = location.id
+            locations=self.env['stock.location'].search([('usage' , '=' , 'production')],limit=1)
+            for location in locations:
+                location_dest_id = location.id
+            if obj.quantite<0:
+                mem_location_id  = location_id
+                location_id      = location_dest_id
+                location_dest_id = mem_location_id
+            obj.location_id = location_id
+            obj.location_dest_id = location_dest_id
 
 
     @api.model
@@ -30,7 +57,7 @@ class is_correction_stock(models.Model):
     def correction_stock_action(self):
         for obj in self:
             if not obj.move_id:
-                reference="%s %s"%(obj.name, obj.commentaire or '')
+                reference="%s %s %s"%(obj.name, obj.cause_id.name or '', obj.commentaire or '')
                 vals={
                     "product_id": obj.product_id.id,
                     "product_uom": obj.product_id.uom_id.id,
@@ -39,7 +66,7 @@ class is_correction_stock(models.Model):
                     "origin": reference,
                     "name": reference,
                     "reference": reference,
-                    "product_uom_qty": obj.quantite,
+                    "product_uom_qty": abs(obj.quantite),
                     "scrapped": False,
                     "propagate_cancel": True,
                     "additional": False,
@@ -51,7 +78,7 @@ class is_correction_stock(models.Model):
                     "product_uom_id": obj.product_id.uom_id.id,
                     "location_id": obj.location_id.id,
                     "location_dest_id": obj.location_dest_id.id,
-                    "qty_done": obj.quantite,
+                    "qty_done": abs(obj.quantite),
                     "reference": reference,
                 }
                 self.env['stock.move.line'].create(vals)
--- models/purchase.py
+++ models/purchase.py
@@ -6,17 +6,15 @@ import datetime
 
 class purchase_order_line(models.Model):
     _inherit = "purchase.order.line"
-    #_order = "is_sequence,id"
 
-    is_sequence   = fields.Integer("Séquence")
-    is_date_ar    = fields.Date("Date AR")
-    is_masse_tole = fields.Float("Masse tôle", related="product_id.is_masse_tole", readonly=True)
-    is_num_ligne  = fields.Integer("N°", help="Numéro de ligne automatique", compute="_compute_is_num_ligne", readonly=True, store=False)
-    is_contrat_id = fields.Many2one('is.contrat.fournisseur', 'Contrat')
-    is_qt_cde     = fields.Float("Qt cde"       , compute='_compute_qt', readonly=True)
-    is_qt_contrat = fields.Float("Qt contrat"   , compute='_compute_qt', readonly=True)
-    is_qt_reste   = fields.Float("Reste contrat", compute='_compute_qt', readonly=True)
-
+    is_sequence      = fields.Integer("Séquence")
+    is_date_ar       = fields.Date("Date AR")
+    is_masse_tole    = fields.Float("Masse tôle", related="product_id.is_masse_tole", readonly=True)
+    is_num_ligne     = fields.Integer("N°", help="Numéro de ligne automatique", compute="_compute_is_num_ligne", readonly=True, store=False)
+    is_contrat_id    = fields.Many2one('is.contrat.fournisseur', 'Contrat')
+    is_qt_cde        = fields.Float("Qt cde"       , compute='_compute_qt', readonly=True)
+    is_qt_contrat    = fields.Float("Qt contrat"   , compute='_compute_qt', readonly=True)
+    is_qt_reste      = fields.Float("Reste contrat", compute='_compute_qt', readonly=True)
 
 
     def get_qt_cde(self, partner_id, product_id, contrat_id):
@@ -48,20 +46,7 @@ class purchase_order_line(models.Model):
             product_id = obj.product_id.id
             contrat_id = obj.is_contrat_id.id
             if product_id and contrat_id and partner_id:
-                qt_cde = self.get_qt_cde(partner_id, product_id, contrat_id)
-                # SQL="""
-                #     SELECT sum(pol.product_qty)
-                #     FROM purchase_order_line pol join purchase_order po on pol.order_id=po.id
-                #     WHERE 
-                #         pol.product_id=%s and 
-                #         pol.is_contrat_id=%s and
-                #         po.partner_id=%s and
-                #         po.state='purchase'
-                #     limit 1
-                # """
-                # cr.execute(SQL,[product_id,contrat_id,partner_id])
-                # for row in cr.fetchall():
-                #     qt_cde = row[0]
+                qt_cde = self.get_qt_cde(partner_id, product_id, contrat_id)               
             if obj.is_contrat_id:
                 for line in obj.is_contrat_id.ligne_ids:
                     if line.product_id.id==product_id:
@@ -175,6 +160,8 @@ class purchase_order(models.Model):
     is_montant_valide        = fields.Float("Montant validé")
     is_valideur_id           = fields.Many2one('res.users', 'Valideur')
     is_adresse_livraison_id  = fields.Many2one("res.partner", "Adresse de livraison")
+    is_sale_order_id         = fields.Many2one('sale.order', 'Commande de vente')
+    is_nom_affaire           = fields.Char(related="is_sale_order_id.is_nom_affaire")
 
 
     def validation_action(self):
@@ -187,8 +174,6 @@ class purchase_order(models.Model):
         for order in self:
             if order.is_alerte_rsp_achat or order.is_alerte_dir_finance:
                 raise ValidationError("Cette commande doit-être validée")
-            #else:
-            #    self.write({'state': 'approved', 'date_approve': datetime.date.today()})
         res = super(purchase_order, self).button_confirm()
         return res
 
@@ -216,14 +201,6 @@ class purchase_order(models.Model):
         return res
 
 
-
-
-
-
-
-
-
-
     def get_nomenclature(self, niveau, quantite, product_tmpl, bom_id=False,multiniveaux=True):
         for obj in self:
             if product_tmpl and quantite:
@@ -232,44 +209,16 @@ class purchase_order(models.Model):
                     for line in bom.bom_line_ids:
                         coef = line.product_uom_id._compute_quantity(1,line.product_id.uom_id )
                         boms2 = self.env['mrp.bom'].search([('product_tmpl_id','=',line.product_id.product_tmpl_id.id)],limit=1)
-                        #type_article="composant"
-                        #if boms2:
-                        #    type_article="compose"
                         product_qty = line.product_qty*quantite
                         vals={
                             "order_id"    : obj.id,
                             "product_id"  : line.product_id.id,
                             "niveau"      : niveau,
                             "product_qty" : product_qty,
-                            #"type_article": type_article
                         }
                         res = self.env['is.purchase.order.nomenclature.line'].create(vals)
                         if multiniveaux:
                             obj.get_nomenclature(niveau+1, product_qty, line.product_id.product_tmpl_id,multiniveaux=multiniveaux)
-
-
-
-
-                # boms = self.env["mrp.bom"].search([("product_tmpl_id","=",product_tmpl.id)], limit=1)
-                # for bom in boms:
-                #     for line in bom.bom_line_ids:
-                #         for seller in line.product_id.seller_ids:
-                #             if seller.name.id == obj.partner_id.id:
-                #                 qty        = line.product_qty*product_qty
-                #                 product_id = line.product_id
-                #                 if product_id not in lines:
-                #                     lines[product_id]=0
-                #                 lines[product_id]+=qty
-
-                #                 vals={
-                #                     "order_id"       : obj.id,
-                #                     "nomenclature_id": obj.id,
-                #                     "sequence"       : sequence,
-                #                     "product_id"     : product_id.id,
-                #                     "niveau"         : product_id.id,
-                #                     "product_qty"    : qty,
-                #                 }
-                #                 res = self.env["purchase.order.line"].create(vals)
 
 
     def import_nomenclature_action(self):
@@ -302,38 +251,6 @@ class purchase_order(models.Model):
                 res = self.env["purchase.order.line"].create(vals)
 
 
-
-
-
-    # def import_nomenclature_action(self):
-    #     for obj in self:
-    #         obj.order_line.unlink()
-    #         lines={}
-    #         for l in obj.is_nomenclature_ids:
-    #             if l.product_id and l.quantite_vendue:
-    #                 boms = self.env["mrp.bom"].search([("product_tmpl_id","=",l.product_id.id)], limit=1)
-    #                 for bom in boms:
-    #                     for line in bom.bom_line_ids:
-    #                         for seller in line.product_id.seller_ids:
-    #                             if seller.name.id == obj.partner_id.id:
-    #                                 qty        = line.product_qty*l.quantite_vendue
-    #                                 product_id = line.product_id
-    #                                 if product_id not in lines:
-    #                                     lines[product_id]=0
-    #                                 lines[product_id]+=qty
-    #         sequence=0
-    #         for product_id in lines:
-    #             sequence+=10
-    #             qty = lines[product_id]
-    #             vals={
-    #                 "order_id"   : obj.id,
-    #                 "is_sequence": sequence,
-    #                 "product_id" : product_id.id,
-    #                 "product_qty": qty,
-    #             }
-    #             res = self.env["purchase.order.line"].create(vals)
-
-
     def mouvement_stock_action(self):
         for obj in self:
             ids=[]
@@ -360,43 +277,3 @@ class purchase_order(models.Model):
 
 
 
-
-
-    # #TODO : J"ai surchargé cette fonction le 28/11/19, car le modèle de mail de la demande de prix a été supprimée
-    # def wkf_send_rfq(self, cr, uid, ids, context=None):
-    #     """
-    #     This function opens a window to compose an email, with the edi purchase template message loaded by default
-    #     """
-    #     if not context:
-    #         context= {}
-    #     ir_model_data = self.pool.get("ir.model.data")
-    #     try:
-    #         if context.get("send_rfq", False):
-    #             template_id = ir_model_data.get_object_reference(cr, uid, "is_bsa", "is_demande_de_prix_email_template")[1]
-    #         else:
-    #             template_id = ir_model_data.get_object_reference(cr, uid, "purchase", "email_template_edi_purchase_done")[1]
-    #     except ValueError:
-    #         template_id = False
-    #     try:
-    #         compose_form_id = ir_model_data.get_object_reference(cr, uid, "mail", "email_compose_message_wizard_form")[1]
-    #     except ValueError:
-    #         compose_form_id = False 
-    #     ctx = dict(context)
-    #     ctx.update({
-    #         "default_model": "purchase.order",
-    #         "default_res_id": ids[0],
-    #         "default_use_template": bool(template_id),
-    #         "default_template_id": template_id,
-    #         "default_composition_mode": "comment",
-    #     })
-    #     return {
-    #         "name": _("Compose Email"),
-    #         "type": "ir.actions.act_window",
-    #         "view_type": "form",
-    #         "view_mode": "form",
-    #         "res_model": "mail.compose.message",
-    #         "views": [(compose_form_id, "form")],
-    #         "view_id": compose_form_id,
-    #         "target": "new",
-    #         "context": ctx,
-    #     }
